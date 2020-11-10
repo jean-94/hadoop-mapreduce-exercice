a59e37f0e37cea6d5bbcee3472f5b09941fc7d1b
theo-f-marechal <73380218+theo-f-marechal@users.noreply.github.com>
2020-11-10 18:46:26 +0100

    add reducer test

---
+++ hadoop-examples-mapreduce/src/test/java/com/opstty/reducer/DistrictReducerTest.java
@@ -0,0 +1,39 @@
+package com.opstty.reducer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DistrictReducerTest {
+    @Mock
+    private Reducer.Context context;
+    private DistrictReducer DistrictReducer;
+
+    @Before
+    public void setup() {
+        this.DistrictReducer = new DistrictReducer();
+    }
+
+    @Test
+    public void testReduce() throws IOException, InterruptedException {
+        String key = "key";
+        NullWritable none = NullWritable.get();
+        IntWritable value = new IntWritable(1);
+        Iterable<IntWritable> values = Arrays.asList(value, value, value);
+        this.DistrictReducer.reduce(new Text(key), values, this.context);
+        verify(this.context).write(new Text(key), none);
+    }
+}
+
