8f0c0fb0d5c7952bd5d4b1ed3c2b669c4c99dd86
jean-94 <jean.ly@efrei.net>
2020-11-10 18:43:35 +0100

    LY Jean - MARECHAL Théo - commint n°2 : Exercice 1.8.5

--- hadoop-examples-mapreduce/src/main/java/com/opstty/mapper/SortHeightMapper.java
+++ hadoop-examples-mapreduce/src/main/java/com/opstty/mapper/SortHeightMapper.java
@@ -1,7 +1,6 @@
 package com.opstty.mapper;
 
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
--- hadoop-examples-mapreduce/src/main/java/com/opstty/reducer/SortHeightReducer.java
+++ hadoop-examples-mapreduce/src/main/java/com/opstty/reducer/SortHeightReducer.java
@@ -1,8 +1,6 @@
 package com.opstty.reducer;
 
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.FloatWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -11,23 +9,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class SortHeightReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
-    private ArrayList<DoubleWritable> list = new ArrayList<>();
-    private ArrayList<DoubleWritable> listwithoutDuplicate = new ArrayList<>();
+    private ArrayList<Double> list = new ArrayList<>();
+    private ArrayList<Double> listwithoutDuplicate = new ArrayList<>();
 
     public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
             throws IOException, InterruptedException {
         for (DoubleWritable val : values) {
-            list.add(val);
+            list.add(val.get());
         }
         Collections.sort(list);
-        for (DoubleWritable val : list) {
-            context.write(key, val);
-            /*
+        for (Double val : list) {
             if (!listwithoutDuplicate.contains(val)) {
                 listwithoutDuplicate.add(val);
-                context.write(key, val);
+                context.write(new Text(""), new DoubleWritable(val));
             }
-             */
         }
     }
 }
