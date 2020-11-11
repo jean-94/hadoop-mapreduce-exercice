95abd00966696b08a8975b2a1f8ddd8aa3c89e28
theo-f-marechal <73380218+theo-f-marechal@users.noreply.github.com>
2020-11-11 17:24:11 +0100

    mod reducer plus test

--- hadoop-examples-mapreduce/src/main/java/com/opstty/reducer/DistrictOldestTreeReducer.java
+++ hadoop-examples-mapreduce/src/main/java/com/opstty/reducer/DistrictOldestTreeReducer.java
@@ -1,27 +1,26 @@
 package com.opstty.reducer;
 
 import com.opstty.AgeDistrictWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
-public class DistrictOldestTreeReducer extends Reducer<Text, AgeDistrictWritable, Text, AgeDistrictWritable> {
+public class DistrictOldestTreeReducer extends Reducer<Text, AgeDistrictWritable, Text, IntWritable> {
 
-    private AgeDistrictWritable result = new AgeDistrictWritable();
-    private boolean first = true;
+    private int result_age = -1;
+    private int result_district = -1;
 
     public void reduce(Text key, Iterable<AgeDistrictWritable> values, Context context)
             throws IOException, InterruptedException {
 
         for (AgeDistrictWritable val : values) {
-            if(!first && (val.getAge() > result.getAge())) {
-                result = val;
-            }else{
-                result = val;
-                this.first = false;
+            if(val.getAge() > result_age) {
+                result_age = val.getAge();
+                result_district = val.getDistrict();
             }
         }
-        context.write(key, result);
+        context.write(key, result_district);
     }
 }
